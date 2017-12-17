574472f1bab3f703d74cfef88583af956897e513
ptl93 <tuan.le@campus.lmu.de>
2017-12-17 12:01:43 +0100

    changed classtype checking and added "reportDataType" class-attribute to output object

--- R/getDataType.R
+++ R/getDataType.R
@@ -5,30 +5,29 @@ getDataType = function(data) {
   fact = vector(mode = "character")
   char = vector(mode = "character")
   logic = vector(mode = "character")
-  date = vector(mode = "character")  
+  date = vector(mode = "character")
   #Loop through each column and get class
   for (i in 1:ncol(data)) {
     coldata = data[,i]
     colname = colnames(data)[i]
-    type = class(coldata)
-    print(type)
-    #Problems: is.numeric(1L) and is.numeric(1) both true.. really need to seperate between int and num
-    if (type == "numeric") {
+    if (is.numeric(coldata)) {
       num   = c(num, colname)
-    } else if (type == "integer") {
+    } else if (is.integer(coldata)) {
       int = c(int, colname)
-    } else if (is.factor(coldata) || is.ordered(coldata)) { ##ordered factor has to be improved..
+    } else if (is.factor(coldata) || is.ordered(coldata)) {
       fact  = c(fact, colname)
-    } else if (type == "character") {
+    } else if (is.character(coldata)) {
       char  = c(char, colname)
-    } else if (type == "logical") {
+    } else if (is.logical()) {
       logic = c(logic, colname)
-    } else if (type == "Date") {
+    } else if (class(coldata) == "Date") {
       date = c(date, colname)
     }
   }
   typelist = list(num = num, int = int, fact = fact,
     char = char, logic = logic, date = date)
+  class(typelist) = append(class(typelist), "reportDataType")
+  return(typelist)
 }
 
 ##testing
@@ -36,7 +35,10 @@ data("diamonds", package = "ggplot2")
 data("Boston", package = "MASS")
 data("iris")
 
-diamondstype = getDataType(diamonds) #spuckt fehler aus
+diamondstype = getDataType(diamonds)
+diamondstype
 iristype = getDataType(iris)
+iristype
 bostontype = getDataType(Boston)
+bostontype
 
