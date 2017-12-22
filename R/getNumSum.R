6aaa2af5d8baff14694f34684f0d9312d1c0c7a1
ptl93 <tuan.le@campus.lmu.de>
2017-12-22 15:02:35 +0100

    finalized getNumSum. Fixed lint. Need to add plots

--- R/getNumSum.R
+++ R/getNumSum.R
@@ -3,21 +3,21 @@ getNumSum = function(data, features) {
   #for the next calculations NAs will be removed
   no.obs = apply(num.data, 2, function(x) sum(!is.na(x)))
   nas = apply(num.data, 2, function(x) sum(is.na(x)))
+  nas.perc = nas / nrow(num.data)
   mean = apply(num.data, 2, function(x) mean(x, na.rm = TRUE))
   kurtosis = apply(num.data, 2, function(x) moments::kurtosis(x, na.rm = TRUE))
   skewness = apply(num.data, 2, function(x) moments::skewness(x, na.rm = TRUE))
   sd = apply(num.data, 2, function(x) sd(x, na.rm = TRUE))
   min = apply(num.data, 2, function(x) min(x, na.rm = TRUE))
-  q0.25 = apply(num.data, 2, function(x) stats::quantile(x, probs = 0.25, na.rm = TRUE))
-  median = apply(num.data, 2, function(x) stats::median(x, na.rm = TRUE))
-  q0.75 = apply(num.data, 2, function(x) stats::quantile(x, probs = 0.75, na.rm = TRUE))
+  quantiles = apply(num.data, 2, function(x) stats::quantile(x, probs = c(.01, .05, .1, .25, .5, .75, .9, .95, .99), na.rm = TRUE))
   max = apply(num.data, 2, function(x) max(x, na.rm = TRUE))
   range = max - min
   iqr = apply(num.data, 2, function(x) stats::IQR(x, na.rm = TRUE))
-  l.bound = q0.25 - 1.5*iqr
-  u.bound = q0.25 + 1.5*iqr
+  l.bound = quantiles[4, ] - 1.5 * iqr
+  u.bound = quantiles[6, ] + 1.5 * iqr
   no.outliers = sapply(colnames(num.data), FUN = function(x) length(which(num.data[[x]] < l.bound[x] | num.data[[x]] > u.bound[x])))
   no.zero = apply(num.data, 2, function(x) length(which(x == 0)))
   no.unique = apply(num.data, 2, function(x) length(unique(x)))
-  #num.sum.df =
+  num.sum.df = t(as.data.frame(rbind(no.obs, nas, nas.perc, mean, kurtosis, skewness, sd, min, quantiles, max, range, iqr,
+    l.bound, u.bound, no.outliers, no.zero, no.unique)))
 }
