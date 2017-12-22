1106b74cf9f2ef5ab8cd4231af4ac8c2a11fe164
ptl93 <tuan.le@campus.lmu.de>
2017-12-22 11:32:58 +0100

    first version of makeNumSum

---
+++ R/makeNumSum.R
@@ -0,0 +1,29 @@
+#' @title Calculates a Numeric Summary
+#'
+#' @description
+#' makeNumSum calculates a numeric summary and wrapes an object around it
+#'
+#' @param corr.task [\code{CorrTask}]\cr
+#'   A NumSumTask Object
+#' @return NumSumObj
+#' @examples
+#'  data("Boston", package = "MASS")
+#'  num.sum.task = makeNumSumTask(id = "BostonTask", data = Boston, target = "medv")
+#'  #get the numeric summary task object
+#'  num.sum = makeNumSum(numSum.task)
+#' @export
+#' @import checkmate
+#' @import BBmisc
+#' @import moments
+#' @export
+makeNumSum = function(num.sum.task){
+  assertClass(num.sum.task, "NumTask") #ToDO: change to NumSumTask (class) for more clarification (hence change makeNumSumTask.R)
+
+  data = num.sum.task$env$data
+  features = unlist(num.sum.task$numdatatypes)
+  num.sum = "getNumSum(num.sum.task$env$data, unlist(numSum.task$numdatatypes))" #toDo : create this function
+
+  makeS3Obj("NumSumObj",
+    num.sum = num.sum,
+    task = num.sum.task)
+}
