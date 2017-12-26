6927d8cc29f4dcfad35a631e1bce4418bb78f77f
ptl93 <tuan.le@campus.lmu.de>
2017-12-26 21:11:21 +0100

    add aditional ggplot

--- R/naSummary.R
+++ R/naSummary.R
@@ -12,8 +12,8 @@
 #'   A numeric value which defines the margin size of the left. For more information see \link[graphics]{par}.
 #' @param report.task [\code{ReportTaskObj}]\cr
 #'   A Report Task Object.
-#' @return A [\code{naSumObj}] with Names of the variables with their frequency of missing values and an additional plot
-#'   which shows the position of the missing values (color = black) for each variable with NAs.
+#' @return A [\code{naSumObj}] with Names of the variables with their frequency of missing values and two additional plots
+#'   which shows the position of the missing values (color = black) for each variable with NAs and the number of missing values as a bar plot
 #' @examples
 #'  data("airquality")
 #'  #create new columns
@@ -36,6 +36,9 @@
 #' @export
 #' @import checkmate
 #' @import BBmisc
+#' @import ggplot2
+#' @import grid
+#' @import gridBase
 #' @title Giving a NA summary and an image of a data with missing values
 
 summaryNA  = function(data, show.plot = FALSE, show.result = FALSE, margin.left = 4, report.task = NULL){
@@ -46,28 +49,32 @@ summaryNA  = function(data, show.plot = FALSE, show.result = FALSE, margin.left 
 
   num = as.numeric(which(apply(is.na(data), 2, any)))
 
-  if (length(num) > 0){
+  if (length(num) > 0) {
 
-    na.summary = numeric(length(num))
-    names(na.summary) = colnames(data)[num]
-    na.summary = colSums(is.na(data[, num, drop = FALSE]))
-    if (show.result){
-      cat("In total there are:", sum(na.summary), "NAs in the dataset:", deparse(substitute(data)), "\n")
-      print(na.summary)
+    na.df = data.frame("feature" = names(data),
+      "num_missing" = sapply(data, function(x) {sum(is.na(x))}),
+      "pct_missing" = sapply(data, function(x) {sum(is.na(x))}) / nrow(data), row.names = NULL)
+    na.df = na.df[with(na.df, order(-num_missing)), ]
+
+    if (show.result) {
+      cat("In total there are:", sum((na.df$num_missing)), "NAs in the dataset:", deparse(substitute(data)), "\n")
+      print(na.df)
     }
     #get the data containing the columns with NAs
     data.new = data[, num, drop = FALSE]
     #flag the NAs as 1
     color = apply(data.new, 2, function(x) as.integer(is.na(x)))
-
+    #order column from largest NAs to lower
+    color = color[, as.character(subset(na.df, num_missing > 0)[["feature"]])]
+    data.new = data.new[, as.character(subset(na.df, num_missing > 0)[["feature"]])]
     #create image function :
     image.code = function() {
-      image(color, col = c("white", "black"), yaxt = "n")
+      image(color, col = c("white", "black"), yaxt = "n", xlab = "Index from observation", xaxt = "n")
       par(mar = c(5, margin.left, 4, 2) + 0.1)
       abline(v = -0.001)
       abline(h = 1.015)
 
-      if (length(num) == 1){
+      if (length(num) == 1) {
         y.type = 0
       } else {
         y.type = 0:(ncol(data.new) - 1) / (length(data.new) - 1)
@@ -76,15 +83,37 @@ summaryNA  = function(data, show.plot = FALSE, show.result = FALSE, margin.left 
       axis(2, labels = colnames(data.new), at = y.type, las = 2)
     }
 
-    if (show.plot){
+    #create na.ggplot
+    na.ggplot = ggplot(subset(na.df, num_missing > 0), aes_string(x = "feature", y = "num_missing")) +
+      geom_bar(stat = "identity", colour = "black", alpha = 0.4) +
+      geom_text(aes(label = paste0(round(100 * pct_missing, 0), "%")), hjust = -0.15, size = 3.5) +
+      scale_y_continuous(labels = waiver()) +
+      theme(legend.position = "bottom") + coord_flip() +
+      xlab("Features") + ylab("Number of missing values")
+
+    if (show.plot) {
+      #Create figure window and layout
+      plot.new()
+      grid.newpage()
+      pushViewport(viewport(layout = grid.layout(2, 1)))
+
+      #Draw ggplot
+      pushViewport(viewport(layout.pos.row = 1))
+      print(na.ggplot, newpage = FALSE)
+      popViewport()
+
+      #Draw base plot
+      pushViewport(viewport(layout.pos.row = 2))
+      par(fig = gridFIG(), new = TRUE)
       image.code()
+      popViewport()
     }
     env = new.env(parent = emptyenv())
     env$color = color
     env$data.new = data.new
     env$margin.left = margin.left
     env$num = num
-    makeS3Obj("naSumObj", nas = na.summary, data = data, dataset.name = deparse(substitute(data)), env = env,
+    makeS3Obj("naSumObj", na.df = na.summary, data = data, dataset.name = deparse(substitute(data)), env = env,
       image = function() {
         image(env$color, col = c("white", "black"), yaxt = "n")
         par(mar = c(5, env$margin.left, 4, 2) + 0.1)
@@ -99,12 +128,13 @@ summaryNA  = function(data, show.plot = FALSE, show.result = FALSE, margin.left 
         axis(2, labels = colnames(env$data.new), at = env$y.type, las = 2)
         #remove y.type from the environment
         rm(y.type, envir = env)
-      })
+      }, ggplot = na.ggplot)
 
   }
   else{
     if (show.result) cat("There are no missing values in the dataset: ", deparse(substitute(data)), "\n")
-    makeS3Obj("naSumObj", nas = NULL, data = data, dataset.name = deparse(substitute(data)), env = NULL, image = NULL)
+    makeS3Obj("naSumObj", nas = NULL, data = data, dataset.name = deparse(substitute(data)), env = NULL, image = NULL,
+      ggplot = NULL)
   }
 }
 
@@ -116,6 +146,7 @@ print.naSumObj = function(x, ...) {
   } else {
     cat("In total there are", sum(x$nas), "NAs in the dataset:", x$dataset.name, "\n")
     print(x$nas)
+    cat("Printing image object with NAs according to observation index: \n")
     image(x$env$color, col = c("white", "black"), yaxt = "n")
     par(mar = c(5, x$env$margin.left, 4, 2) + 0.1)
     abline(v = -0.001)
@@ -129,5 +160,7 @@ print.naSumObj = function(x, ...) {
     axis(2, labels = colnames(x$env$data.new), at = x$env$y.type, las = 2)
     #remove y.type from the environment
     rm(y.type, envir = x$env)
+    cat("Printing ggplot object according to number of missing values")
+    print(x$ggplot)
   }
 }
