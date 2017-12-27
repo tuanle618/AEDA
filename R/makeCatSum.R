0e6b60ede001820a30b426382199d8eae710a296
ptl93 <tuan.le@campus.lmu.de>
2017-12-27 23:16:07 +0100

    first version of makeCatSum

---
+++ R/makeCatSum.R
@@ -0,0 +1,39 @@
+#' @title Calculates a Categorical Summary
+#'
+#' @description
+#' makeCatSum calculates a categorical summary and wrapes an object around it
+#'
+#' @param cat.sum.task [\code{CatSumTask}]\cr
+#'   A CatSumTask Object
+#' @return CatSumObj
+#' @examples
+#'  data("Arthritis", package = "vcd")
+#'  cat.sum.task = makeCatSumTask(id = "Arthritis.Task", data = Arthritis, target = "Improved")
+#'  #get the categorical summary task object
+#'  cat.sum = makeCatSum(cat.sum.task)
+#' @import checkmate
+#' @import BBmisc
+#' @import moments
+#' @import stats
+#' @import vcd
+#' @export
+makeCatSum = function(cat.sum.task){
+  assertClass(cat.sum.task, "CatSumTask")
+
+  data = cat.sum.task$env$data
+  features = unlist(cat.sum.task$catdatatypes)
+  target = cat.sum.task$env$datatypes$target
+  #cat.sum = getCatSum(data, features, target) #function to be done
+
+  makeS3Obj("CatSumObj",
+    cat.sum = NULL,
+    task = cat.sum.task)
+}
+
+#' @export
+# Print function for CatSum Object
+print.CatSumObj = function(x, ...) {
+  catf("Result of categorical summary for: %s", x$task$id)
+  cat("\n")
+  print(x$cat.sum)
+}
