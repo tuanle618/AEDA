07c06220a272c88fe2f11d512d7a992b2c7ef62d
ptl93 <tuan.le@campus.lmu.de>
2018-01-25 10:56:11 +0100

    add multiplot call in rmd

--- R/writeCatSumReport.R
+++ R/writeCatSumReport.R
@@ -55,7 +55,8 @@ writeReport.CatSumReport = function(cat.sum.report, sub.dir = "Data_Report", sav
     writeLines("```{r}", con = report.con)
     writeLines(paste0(cat.sum.report$report.id, "$cat.sum$freq"), con = report.con)
     writeLines(paste0(cat.sum.report$report.id, "$cat.sum$contg.list"), con = report.con)
-    writeLines(paste0(cat.sum.report$report.id, "$cat.sum$contg.list"), con = report.con)
+    writeLines(paste0("multiplot(",cat.sum.report$report.id, "$cat.sum$plot.list", ", cols = 2)"),
+      con = report.con)
     writeLines("```", con = report.con)
 
   }, finally = {
