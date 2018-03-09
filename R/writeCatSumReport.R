89745d991e1aee5cdcefd405e880a50096e738d2
ptl93 <tuan.le@campus.lmu.de>
2018-03-09 09:33:44 +0100

    bugfix multiplot in writeCatSumReport

--- R/writeCatSumReport.R
+++ R/writeCatSumReport.R
@@ -53,7 +53,7 @@ writeReport.CatSumReport = function(cat.sum.report, sub.dir = "Data_Report", sav
     writeLines("```{r, echo=FALSE}", con = report.con)
     writeLines(paste0(cat.sum.report$report.id, "$cat.sum$freq"), con = report.con)
     writeLines(paste0(cat.sum.report$report.id, "$cat.sum$contg.list"), con = report.con)
-    writeLines(paste0("multiplot(",cat.sum.report$report.id, "$cat.sum$plot.list", ", cols = 2)"),
+    writeLines(paste0("multiplot(plotlist = ",cat.sum.report$report.id, "$cat.sum$plot.list", ", cols = 2)"),
       con = report.con)
     writeLines("```", con = report.con)
 
