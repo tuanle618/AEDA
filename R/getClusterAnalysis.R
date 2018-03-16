8e829e09c54f6576d822446980f0a2217d16f374
ptl93 <tuan.le@campus.lmu.de>
2018-03-16 10:07:14 +0100

    edit kmeans and add pam algorithm

--- R/getClusterAnalysis.R
+++ R/getClusterAnalysis.R
@@ -27,75 +27,88 @@
 #' @import factoextra
 #'
 getClusterAnalysis = function(data, num.features, method, par.vals, random.seed, scale.num.data) {
+  ##### http://www.sthda.com/english/wiki/print.php?id=239 #####
   #select numeric data
   set.seed(random.seed)
   num.data = data[, num.features]
   if (scale.num.data) num.data = scale(num.data)
   #tupel combinations
   combinations = combn(x = 1:ncol(num.data), m = 2)
-  if (method == "cluster.kmeans") {
+
+  ##K-Means or PAM:
+  if (is.element(method, c("cluster.kmeans", "cluster.pam"))) {
     ###### all numeric colums together: ######
+    ##asign function to hand over to fviz_nbclust
+    if (method == "cluster.kmeans") {
+      cluster.method = kmeans
+    } else cluster.method = pam
+
     ###Optimal amount of clusters:
-    wss = (nrow(num.data) - 1)*sum(apply(num.data, 2, var))
-    plotRMD = FALSE #placeholder
-    if (plotRMD) {
-      for (i in 2:15) wss[i] = sum(kmeans(num.data,
-        centers = i, iter.max = 20L)$withinss)
-      ##Elbowplot
-      plot(1:15, ((wss[1] - wss)*100)/wss[1], type = "b", xlab = "Number of clusters",
-        ylab = "Amount of total variance in %", ylim = c(0,100), main = "Elbowplot")
-      ##Scree-plot
-      plot(1:15, wss, type = "b", xlab = "Number of clusters",
-        ylab = "Within groups sum of squares", main = "Screeplot")
-    }
-    ##rather work with gg-plot-objects provided in factorextra and Nbclust pkg
-    #plots also with:
-    #silhouette
-    nb.silh = fviz_nbclust(num.data, kmeans, method = "silhouette") +
+    ##work with gg-plot-objects provided in factorextra and Nbclust pkg
+
+    # Silhouette-method
+    nb.silh = fviz_nbclust(x = num.data, FUNcluster = cluster.method, method = "silhouette") +
       labs(subtitle = "Silhouette method")
     #extract optim amount of clusters according to silhouette method
     optim.silh = as.integer(as.character(nb.silh$data[which.max(nb.silh$data$y), 1]))
-    # Gap statistic
-    nb.gap = fviz_nbclust(num.data, kmeans, nstart = 25,  method = "gap_stat", nboot = 100, verbose = FALSE) +
+
+    # Gap-method
+    nb.gap = fviz_nbclust(x = num.data, FUNcluster = cluster.method,  method = "gap_stat", nboot = 100, verbose = FALSE) +
       labs(subtitle = "Gap statistic method")
-    #all Nbclust indices considering: !!! takes long to execute... !!!
-    nb.all = invisible(NbClust(num.data, distance = "euclidean", min.nc = 2,
-      max.nc = 10, method = "kmeans"))
-    res.oa = fviz_nbclust(nb.all)
-    optim.amount = as.numeric(sub("\\D+", "", res.oa$labels$title))
+    # ToDo: function to extract optim number of cluster. Subsection 'Algorithm 5.4 on website'
 
-    ##do the cluster with optim amount:
-    optim.cluster = kmeans(num.data, centers = optim.amount)
+    nb.wss = fviz_nbclust(num.data, cluster.method, method = "wss") +
+      labs(subtitle = "Elbow method")
 
+    ##do the cluster with optim silhouette:
+    if (method == "cluster.kmeans") {
+      optim.cluster = kmeans(x = num.data, centers = optim.silh, iter.max = 30L)
+    } else optim.cluster = pam(x = num.data, k = optim.silh)
+    ##plot cluster: NOTE: PCA with 2principal components applied:
+    pca.cluster.plot = fviz_cluster(optim.cluster, data = num.data, geom = "point",
+      stand = FALSE, ellipse.type = "norm")
     ##save results for all numeric data:
-    cluster.all = list(nb.silh = nb.silh, nb.gap = nb.gap, nb.all = nb.all, res.oa = res.oa,
-      optim.amount = optim.amount, optim.cluster = optim.cluster) ##include optim.amount.silh?!
+    cluster.all = list(nb.silh = nb.silh, nb.gap = nb.gap, nb.wss = nb.wss,
+      optim.amount = optim.silh, optim.cluster = optim.cluster, pca.cluster.plot = pca.cluster.plot)
 
     ###### clusters for combinations: ######
     comb.cluster.list = apply(combinations, 2, function(x) {
       #print(x)
       cols = colnames(num.data)[x]
-      #placeholder
+      #placeholder: NbClust takes too much time
       if (FALSE) {
         nb = invisible(NbClust(num.data[, x], distance = "euclidean", min.nc = 2,
           max.nc = 10, method = "kmeans"))
         res.oa = fviz_nbclust(nb)
         optim.amount = as.numeric(sub("\\D+", "", res.oa$labels$title))
       }
-      nb.silh = fviz_nbclust(num.data[, x], kmeans, method = "silhouette") +
-      labs(subtitle = "Silhouette method")
+      ##gap:
+      nb.gap = fviz_nbclust(x = num.data[, x], FUNcluster = cluster.method,  method = "gap_stat", nboot = 100, verbose = FALSE) +
+        labs(subtitle = "Gap statistic method")
+      ##elbow:
+      nb.wss = fviz_nbclust(num.data[, x], FUNcluster = cluster.method, method = "wss") +
+        labs(subtitle = "Elbow method")
+      ##silhouette:
+      nb.silh = fviz_nbclust(x = num.data[, x], FUNcluster = cluster.method, method = "silhouette") +
+        labs(subtitle = "Silhouette method")
       #extract optim amount of clusters according to silhouette method
       optim.silh = as.integer(as.character(nb.silh$data[which.max(nb.silh$data$y), 1]))
-      ##apply cluster with optim:
-      optim.cluster = kmeans(num.data[, x], centers = optim.silh)
-      ##ggplot cluster
+      ##do the cluster with optim silhouette:
+      if (method == "cluster.kmeans") {
+        optim.cluster = kmeans(x = num.data[, x], centers = optim.silh, iter.max = 30L)
+      } else optim.cluster = pam(x = num.data[, x], k = optim.silh)
+      ##ggplot clusterw
       cluster.plot = fviz_cluster(optim.cluster, data = num.data[, x], geom = "point",
         stand = FALSE, ellipse.type = "norm")
       ##store results:
-      list(names = cols, nb.silh = nb.silh, optim.amount = optim.silh,
+      list(names = cols, nb.gap = nb.gap, nb.wss = nb.wss, nb.silh = nb.silh, optim.amount = optim.silh,
         optim.cluster = optim.cluster, cluster.plot = cluster.plot)
     })
+
+    ##Hierarchical clustering:
   } else if (method == "cluster.h") {
+
+
 
   }
 out.list = list(cluster.all = cluster.all, comb.cluster.list = comb.cluster.list)
