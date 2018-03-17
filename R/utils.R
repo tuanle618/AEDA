57d3cf80a72b6bfe464ec8af409c7b31727f76d8
Michael Graber <m.graber@campus.lmu.de>
2018-03-17 12:42:26 +0100

    rmdLibrary checks for installed pkgÂ

--- R/utils.R
+++ R/utils.R
@@ -39,7 +39,11 @@ makeS3Obj2 = function(classes, object, ...){
 
 # Wrapper for loading not already loaded librarys
 rmdLibrary = function(needed.pkgs, file, force = FALSE){
-  catf("require(%s)\n", needed.pkgs, file = file)
+  catf('if (!require("%s",character.only = TRUE))
+    {
+    install.packages("%s",dep=TRUE)
+    if(!require("%s",character.only = TRUE)) stop("Package not found")\n
+    }', needed.pkgs, needed.pkgs, needed.pkgs, file = file)
 }
 
 # Wrapper for loading data
