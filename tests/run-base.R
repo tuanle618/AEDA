c5ee148bb9168d00e0f36ae86c97f2ad221365d8
Michael Graber <m.graber@campus.lmu.de>
2018-03-21 21:19:32 +0100

    test_base_basicReport should fail, 5th try

--- tests/run-base.R
+++ tests/run-base.R
@@ -1,7 +1,7 @@
 library(testthat)
 
 if (identical(Sys.getenv("TRAVIS"), "true")) {
-  test_check("../AEDA", filter = "base_")
+  test_check("AEDA", filter = "base_")
 } else {
   temp.wd = getwd()
   dir.create("TestFolder")
