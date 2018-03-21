8cdc8963166c6f24c83fcf896aef8bb709976da9
Michael Graber <m.graber@campus.lmu.de>
2018-03-21 22:22:17 +0100

    fix test finish report 3rd try

--- tests/run-base.R
+++ tests/run-base.R
@@ -1,4 +1,13 @@
 library(testthat)
 
-test_check("AEDA", filter = "base_")
+if (identical(Sys.getenv("TRAVIS"), "true")) {
+  test_check("AEDA", filter = "base_")
+} else {
+  temp.wd = getwd()
+  dir.create("TestFolder")
+  setwd("TestFolder/")
+  try(test_check("../AEDA", filter = "base_"))
+  setwd(temp.wd)
+  unlink("TestFolder", recursive = TRUE)
+}
 
