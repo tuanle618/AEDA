51aff1e23b01a2a1e8d26d9d61f662cf22132ae3
Michael Graber <m.graber@campus.lmu.de>
2018-03-21 20:18:16 +0100

    fix lints, but travis should fail

--- tests/testthat/test_base_finishReport.R
+++ tests/testthat/test_base_finishReport.R
@@ -9,22 +9,22 @@ test_that("finishReport Works",{
 
   data("Aids2", package = "MASS")
   set.seed(1)
-  Aids2 = Aids2[sample.int(500,100),]
+  aids2 = Aids2[sample.int(500, 100), ]
   # Test with Boston dataset and just two reports
-  num.sum.task = makeNumSumTask(id = "BostonTask", data = Aids2, target = "T.categ")
+  num.sum.task = makeNumSumTask(id = "BostonTask", data = aids2, target = "T.categ")
   num.sum = makeNumSum(num.sum.task)
   num.sum.report = makeNumSumReport(num.sum)
 
-  my.task = makeCorrTask(id = "BostonTask", data = Aids2, type = "CorrPlot")
+  my.task = makeCorrTask(id = "BostonTask", data = aids2, type = "CorrPlot")
   my.corr = makeCorr(my.task)
   corr.report = makeReport(my.corr)
 
-  cat.sum.task = makeCatSumTask(id = "BostonTask", data = Aids2,
+  cat.sum.task = makeCatSumTask(id = "BostonTask", data = aids2,
    target = "T.categ", na.rm = TRUE)
   cat.sum = makeCatSum(cat.sum.task)
   cat.sum.report = makeReport(cat.sum)
 
-  my.report.task = makeBasicReportTask(id = "BostonTask", data = Aids2, target = "sex")
+  my.report.task = makeBasicReportTask(id = "BostonTask", data = aids2, target = "sex")
   basic.report = makeReport(my.report.task)
 
 
