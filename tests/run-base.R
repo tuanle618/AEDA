65965800ada22cc3bd76de81456a7684c7b9a062
Michael Graber <m.graber@campus.lmu.de>
2018-03-21 21:09:40 +0100

    fix folder; test_base_basicReport should fail

--- tests/run-base.R
+++ tests/run-base.R
@@ -3,9 +3,9 @@ temp.wd = getwd()
 dir.create("TestFolder")
 setwd("TestFolder/")
 if (identical(Sys.getenv("TRAVIS"), "true")) {
-  test_check("AEDA", filter = "base_")
+  test_check("../AEDA", filter = "base_")
 } else {
-  try(test_check("AEDA", filter = "base_"))
+  try(test_check("../AEDA", filter = "base_"))
 }
 setwd(temp.wd)
 unlink("TestFolder", recursive = TRUE)
