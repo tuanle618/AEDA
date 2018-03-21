2a96d4655fbd48b88a0e87f57364ee8011a0ecd9
Michael Graber <m.graber@campus.lmu.de>
2018-03-21 20:48:08 +0100

    travis should fail;was try a problem

--- tests/run-base.R
+++ tests/run-base.R
@@ -2,6 +2,10 @@ library(testthat)
 temp.wd = getwd()
 dir.create("TestFolder")
 setwd("TestFolder/")
-try(test_check("AEDA", filter = "base_"))
+if (identical(Sys.getenv("TRAVIS"), "true")) {
+  test_check("AEDA", filter = "base_")
+} else {
+  try(test_check("AEDA", filter = "base_"))
+}
 setwd(temp.wd)
 unlink("TestFolder", recursive = TRUE)
