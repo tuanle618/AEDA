6630b9e20a6bbdafd029b9a349b4a98d6a50ff45
Michael Graber <m.graber@campus.lmu.de>
2018-03-24 02:01:06 +0100

    small incosistensy fix makeCatSumReport; added tests for CatSum

--- tests/testthat/test_base_getNumSum.R
+++ tests/testthat/test_base_getNumSum.R
@@ -5,7 +5,7 @@ test_that("getNumSum", {
   expect_warning({num.sum = getNumSum(data = airquality, target = "Wind",
     c("Temp", "Ozone", "Solar.R"),  geom.hist.args = list(bins = 30, alpha = 0.4), geom.dens.args = list(size = 2, alpha = 0.4), geom.box.args = list())})
     expect_list(num.sum, len = 2L)
-  expect_matrix(num.sum$num.sum.df, nrows = 3L , ncols = 25L)
+  expect_matrix(num.sum$num.sum.df, nrows = 3L, ncols = 25L)
   expect_list(num.sum$merged.list, len = 3L)
   lapply(num.sum$merged.list, FUN = function(x){
     expect_equal(length(x), 4L)
