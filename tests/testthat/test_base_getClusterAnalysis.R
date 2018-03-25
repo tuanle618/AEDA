bc9e919a7d1e788ae578e21aa35716ebf2065365
Michael Graber <m.graber@campus.lmu.de>
2018-03-26 00:59:30 +0200

    temporary travis fix, 2nd

--- tests/testthat/test_base_getClusterAnalysis.R
+++ tests/testthat/test_base_getClusterAnalysis.R
@@ -117,7 +117,7 @@ test_that("getClusterAnalysis cluster.dbscan", {
   expect_list(clustered$comb.cluster.list, len = 6L)
 
   clustered.minpts = getClusterAnalysis(data = iris, cluster.cols = NULL, num.features = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
-    random.seed = 1L, par.vals = list(minPts = 2L), scale.num.data = TRUE, method = "cluster.dbscan")
+    random.seed = 1L, par.vals = list(eps=0.6, minPts = 2L), scale.num.data = TRUE, method = "cluster.dbscan")
   # check if clusters are different -> is par.vals working correctly
   expect_identical(clustered.minpts$cluster.all$cluster.res$minPts, 2L)
 })
