eace3e576f71184ab45e24c8c9cf5441c7756ebb
ptl93 <tuan.le@campus.lmu.de>
2018-03-29 11:32:26 +0200

    import devtools for testing single rmd's and try fix latex error

--- R/writeMDSAnalysisReport.R
+++ R/writeMDSAnalysisReport.R
@@ -3,7 +3,8 @@
 #' @param report [\code{MDSAnalysisReport} Object]\cr
 #'   The report Object which should be converted to an rmd file
 #' @param sub.dir [\code{character(1)}]\cr
-#'   the name of the (relative) sub-directory where the data report will be saved. Default is \code{Data_Report}
+#'   the name of the (relative) sub-directory where the data report will be saved.\cr
+#'   Default is \code{Data_Report}
 #' @param save.mode [\code{logical(1)}]\cr
 #'   In Save mode its not possible to use an existing folder.
 #'   To ensure no data is lost, a new folder will be created (if possible).
@@ -21,7 +22,8 @@
 #' @return Invisible NULL
 #' @import checkmate
 #' @export
-writeReport.MDSAnalysisReport = function(report, sub.dir = "Data_Report", save.mode = TRUE, override = FALSE){
+writeReport.MDSAnalysisReport = function(report, sub.dir = "Data_Report",
+  save.mode = TRUE, override = FALSE){
   report.env = new.env(parent = .GlobalEnv)
   assertClass(report, "MDSAnalysisReport")
   assertCharacter(sub.dir, len = 1L, min.chars = 1L)
@@ -38,7 +40,8 @@ writeReport.MDSAnalysisReport = function(report, sub.dir = "Data_Report", save.m
 
     writeLines("## Multidimensional Scaling Summary Report", con  = report.con)
     #Load object and libraries:
-    writeLines(writeRChunkOptions(chunkname = "loadMDSObj", id = getId(report)), con = report.con)
+    writeLines(writeRChunkOptions(chunkname = "loadMDSObj", id = getId(report)),
+      con = report.con)
     rmdLibrary("DT", file = report.con)
     writeLines("devtools::load_all() #temporary", con = report.con)
     # save object and write code to load it in the rmd-file
@@ -47,12 +50,14 @@ writeReport.MDSAnalysisReport = function(report, sub.dir = "Data_Report", save.m
 
     ##Add generic text:
     writeLines("### Multidimensional Scaling Summary Results\n", con = report.con)
-    writeLines("The following data frame shows the created dimensions for numeric columns from the dataset:", con = report.con)
+    writeLines("The following data frame shows the created dimensions for numeric columns from the dataset:",
+      con = report.con)
     #Show result dataframe
     writeLines(writeRChunkOptions(chunkname = "showMDSdf", id = getId(report)), con = report.con)
 
     #plot filterable datatable as option:
-    writeLines(paste0("dt = DT::datatable(", report$report.id, "$mds.analysis$mds.result.data, class = 'compact', filter = 'bottom', options = list(pageLength = 10),
+    writeLines(paste0("dt = DT::datatable(", report$report.id,
+      "$mds.analysis$mds.result.data, class = 'compact', filter = 'bottom', options = list(pageLength = 10),
       caption = htmltools::tags$caption(
       style = 'caption-side: bottom; text-align: center;',
       'Table : ', 'Numeric Summary'
@@ -64,7 +69,8 @@ writeReport.MDSAnalysisReport = function(report, sub.dir = "Data_Report", save.m
     writeLines("### Multidimensional Scaling Plot\n", con = report.con)
     writeLines("In the following the result of MDS will be shown:", con = report.con)
     writeLines(writeRChunkOptions(chunkname = "showMDSPlot", id = getId(report),
-      options = list(echo = FALSE, message = FALSE, warning = FALSE, out.width = "'90%'")), con = report.con)
+      options = list(echo = FALSE, message = FALSE, warning = FALSE, out.width = "'90%'")),
+      con = report.con)
     writeLines(paste0(report$report.id, "$mds.analysis$mds.plot"), con = report.con)
     writeLines("```", con = report.con)
 
