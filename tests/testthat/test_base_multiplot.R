82ad8f03aa66598f5db2559f0f9450170529880c
Michael Graber <m.graber@campus.lmu.de>
2018-03-30 01:22:43 +0200

    added arg checks to multiplot, more plot test changes

--- tests/testthat/test_base_multiplot.R
+++ tests/testthat/test_base_multiplot.R
@@ -10,16 +10,18 @@ gg2 = ggplot(data = airquality, aes(x = Day, y = Wind)) + geom_point()
 gg3 = ggplot(data = airquality, aes(x = Temp, y = Solar.R)) + geom_point()
 gg4 = ggplot(data = airquality, aes(x = Temp, y = Ozone)) + geom_point()
 test_that("multiplot", {
-  multiplot(gg1, gg2, gg3, gg4, layout = matrix(c(1, 2, 3, 4), nrow = 2, byrow = TRUE))
-  expect_error(multiplot(gg1, gg2, gg3, gg4, layout = matrix(c(1, 2, 3, 3), nrow = 2, byrow = TRUE)))
-  multiplot(gg1, gg2, plotlist = list(gg3, gg4))
-  multiplot(gg1, gg2, cols = 2)
-  expect_error(multiplot(gg1, "gg2", cols = 2))
-  expect_error(multiplot(plotlist = list(gg3, "gg4"), cols = 2))
+  suppressWarnings({
+    multiplot(gg1, gg2, gg3, gg4, layout = matrix(c(1, 2, 3, 4), nrow = 2, byrow = TRUE))
+    expect_error(multiplot(gg1, gg2, gg3, gg4, layout = matrix(c(1, 2, 3, 3), nrow = 2, byrow = TRUE)))
+    multiplot(gg1, gg2, plotlist = list(gg3, gg4))
+    multiplot(gg1, gg2, cols = 2)
+    expect_error(multiplot(gg1, "gg2", cols = 2))
+    expect_error(multiplot(plotlist = list(gg3, "gg4"), cols = 2))
+  })
 })
 
 test_that("multiplotPages", {
-  multiplotPages(list(gg1, gg2, gg3, gg4), k = 2, 2)
+  suppressWarnings(multiplotPages(list(gg1, gg2, gg3, gg4), k = 2, 2))
 })
 
 test_that("plotBar", {
